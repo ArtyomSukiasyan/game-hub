774a5c5cf81ca0e233cb32e2f1f5b6a3dc5c56db
Arthur Ghazaryan <newartroll11@gmail.com>
2021-08-25 13:21:59 +0400

    feat:(add game)

---
+++ src/originalGames/Milioner/Milioner.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+import Questions from "./components/questions/Questions";
+import Answers from "./components/answers/Answers";
+import AnswersQuestions from "./anssersQuestions/AnssersQuestions";
+import Bank from "./components/bank/Bank";
+import styles from "./Milioner.module.css";
+
+export default function Milioner() {
+  const [questionNum, setQuestionNum] = useState(0);
+  const [data, setData] = useState(AnswersQuestions[questionNum]);
+  const [point, setPoint] = useState(0);
+
+  const trueAnswer = (id) => {
+    if (id === data.trueAnswerIndex) {
+      console.log(questionNum);
+      setQuestionNum((prevNum) => prevNum + 1);
+      setPoint((prevPoint) => prevPoint + 1);
+    } else {
+      setPoint(0);
+      setQuestionNum(0);
+    }
+  };
+
+  useEffect(() => {
+    setData(AnswersQuestions[questionNum]);
+  }, [questionNum]);
+
+  return (
+    <div className={styles.parentMilioner}>
+      <Bank point={point} />
+      <div className={styles.milioner}>
+        <Questions item={data.question} />
+        <Answers trueAnswer={trueAnswer} item={data.answers} />
+      </div>
+    </div>
+  );
+}
--- src/pages/allGames/AllGames.js
+++ src/pages/allGames/AllGames.js
@@ -2,14 +2,16 @@ import React from "react";
 import Header from "../../components/header/Header";
 import Roulette from "../../originalGames/roulette/Game";
 import Turtles from "../../originalGames/turtlesMemory/Turtles";
+import Milioner from "../../originalGames/Milioner/Milioner";
 
 
 export default function AllGames() {
   return (
     <div>
       <Header />
-      <Roulette />
+      {/* <Roulette /> */}
       {/* <Turtles /> */}
+      <Milioner />
     </div>
   );
 }
