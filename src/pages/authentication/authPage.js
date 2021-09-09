1e25e7c2cecccf4bdadadb1a4af06532c6b7229d
ShavarshP <papoyan101097@mail.ru>
2021-09-09 14:09:42 +0400

    fix: some bage

--- src/pages/authentication/authPage.js
+++ src/pages/authentication/authPage.js
@@ -24,7 +24,7 @@ const Auth = ({ verify, set_loading }) => {
       );
 
       if (userName) {
-        const newData = await request(
+        await request(
           "https://shavarshgame.herokuapp.com/api/generate/",
           "POST",
           { owner: data.userId, userName: userName },
