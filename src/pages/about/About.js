2a0a1ed2b36031e87cec2c74bdb033c3b83f700c
Arthur Ghazaryan <newartroll11@gmail.com>
2021-09-12 13:31:59 +0400

    feat:(about page)

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,9 +1,48 @@
 import React from "react";
+import Person from "../../components/person/Person";
+import styles from "./About.module.css";
+import img1 from "../../img/lg.png";
+import img2 from "../../img/rock.png";
+import img3 from "../../img/turtles.PNG";
+import img4 from "../../img/hangman.png";
 
 export default function About() {
+  const info = [
+    {
+      img: img1,
+      title: "Name Surname",
+    },
+    {
+      img: img2,
+      title: "Name Surname",
+    },
+    {
+      img: img3,
+      title: "Name Surname",
+    },
+    {
+      img: img4,
+      title: "Name Surname",
+    },
+  ];
+
   return (
-    <div>
-      its about us part
-    </div>
+    <section className={styles.sec}>
+      <div className={styles.content}>
+        <div className={styles.teamGroup}>
+          <h3>Meet Our Expert</h3>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the .
+          </p>
+        </div>
+        <div className={styles.teamBx}>
+          {info.map((el) => (
+            <Person img={el.img} title={el.title} />
+          ))}
+        </div>
+      </div>
+    </section>
   );
 }
