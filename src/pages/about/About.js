a5d6a88255d0aabf5de3e26669259bea8062b1ff
Arthur Ghazaryan <newartroll11@gmail.com>
2021-09-13 01:24:35 +0400

    style:(about styles)

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,28 +1,28 @@
 import React from "react";
 import Person from "../../components/person/Person";
 import styles from "./About.module.css";
-import img1 from "../../img/lg.png";
-import img2 from "../../img/rock.png";
-import img3 from "../../img/turtles.PNG";
+import img1 from "../../img/team/img1.jpg";
+import img2 from "../../img/team/img2.jpg";
+import img3 from "../../img/team/img3.jfif";
 import img4 from "../../img/hangman.png";
 
 export default function About() {
   const info = [
     {
       img: img1,
-      title: "Name Surname",
+      title: "Arthur Ghazaryan",
     },
     {
       img: img2,
-      title: "Name Surname",
+      title: "Shavarsh Papoyan ",
     },
     {
       img: img3,
-      title: "Name Surname",
+      title: "Artyom Sukiasyan",
     },
     {
       img: img4,
-      title: "Name Surname",
+      title: "Armine Orujyan",
     },
   ];
 
@@ -30,12 +30,8 @@ export default function About() {
     <section className={styles.sec}>
       <div className={styles.content}>
         <div className={styles.teamGroup}>
-          <h3>Meet Our Expert</h3>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the .
-          </p>
+          <h3>Meet Our Experts</h3>
+          <p>The best friendly experts you'll ever meet.</p>
         </div>
         <div className={styles.teamBx}>
           {info.map((el) => (
